c0fc23c502b9189fd240473941dc68ffee581c62
Victor Kachin <victor.kachin@gmail.com>
2022-01-04 12:07:52 +0300

    refactor: editing caurosel behaviour and styles

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 $(document).ready(function (){
-    $('.slider').slick({
-        arrows: true,
+    $('.responsive').slick({
         dots: true,
-        adaptiveHeight: true,
+        // infinite: false,
+        speed: 1000,
         slidesToShow: 4,
         slidesToScroll: 2,
-        speed: 1000,
+        responsive: [
+            {
+                breakpoint: 1350,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1030,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 710,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
